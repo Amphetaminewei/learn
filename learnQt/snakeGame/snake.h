f67f8e6281a1840c5b7e348bb7ca3ee1d7b2d5de
AmphetamineWei <weiwei@amphetaminewei.win>
2019-07-29 21:41:38 +0800

    qt day 16

--- learnQt/snakeGame/snake.h
+++ learnQt/snakeGame/snake.h
@@ -9,6 +9,12 @@ class Snake : public QGraphicsItem
 public:
     Snake();
     Snake(QObject *parent);
+
+    QRectF boundingRect() const;
+    QPainterPath shape() const;
+
+    //使用QPainter将图形画出来
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *);
 };
 
 #endif // SNAKE_H
